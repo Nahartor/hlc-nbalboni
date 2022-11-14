e85de0de8d689236b537dcdb1f3dc36f7848fcbd
Nahartor <nicobalbonipalma@gmail.com>
2022-11-14 23:22:07 +0100

    Trabajo fin de semana 13/11

--- ahorcado/ahorcado.py
+++ ahorcado/ahorcado.py
@@ -5,24 +5,36 @@
 # Previo
 import random
 
+
 # Funciones
 # Comprobar posiciones acertadas (EN PROCESO)
-def comprobar(intento,desglose):
+def comprobar(intento,deletreo):
     posiciones=[]
-
-    for posicion in desglose:
-        if intento == posicion:
+    num=0
+    for letra in deletreo:
+        if intento == letra:
             posiciones.append(num)
             num=num+1
-
-        elif desglose[3]:
-            seguir=False
-
         else:
             num=num+1
-
     return posiciones
 
+palabraPintada=[]
+def pintarPalabra(palabra,prueba):
+    seguir=True
+    
+    num=0
+    while seguir==True:
+        if num in prueba:
+            palabraPintada.append(palabra[num])
+            num=num+1
+        elif num > len(palabra)-1:
+            seguir=False
+        else:
+            palabraPintada.append("_")
+            num=num+1
+
+    return palabraPintada
 
 #Comienzo del script
 # Genero palabra
@@ -30,12 +42,12 @@ palabras=("PERRO","GATO","COCHE","PYTHON","SISTEMA","PROGRAMA")
 palabra=random.choice(palabras)
 
 # Desglosar la palabra
-desglose=[]
+deletreo=[]
 for letra in palabra:
-    desglose.append(letra)
+    deletreo.append(letra)
 
 # Vamos a dejar aqui esto para comprobar que todo va bien durante el proceso
-print(desglose)
+print(deletreo)
 
 # Presentación del juego
 print("-----:VAMOS A JUGAR AL AH_RCA_O:-----")
@@ -44,11 +56,14 @@ print("-----:VAMOS A JUGAR AL AH_RCA_O:-----")
 intento=input("Escribe una letra (en MAYUSCULAS): ")
 
 # Compruebo que la letra(intento) elegida por el jugador está en la palabra
-if intento in desglose:
+if intento in deletreo:
     print("Bien")
-    prueba=comprobar(intento,desglose)
-
+    
+    prueba=comprobar(intento,deletreo)
     print(prueba)
+
+    final=pintarPalabra(palabra,prueba)
+    print(final)
 
 
 else:
