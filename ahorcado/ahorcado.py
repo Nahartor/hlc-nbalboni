46bdb5e46bee767b664680f890894eec92b15073
lpmoreno <112974392+lpmoreno@users.noreply.github.com>
2022-11-23 19:47:13 +0100

    Update ahorcado.py

--- ahorcado/ahorcado.py
+++ ahorcado/ahorcado.py
@@ -4,7 +4,7 @@
 
 # Previo
 import random
-
+# lpaneque: Entiendo que este es el juego final, cierto?
 
 # Funciones
 # Comprobar posiciones acertadas (EN PROCESO)
@@ -42,7 +42,7 @@ palabras=("PERRO","GATO","COCHE","PYTHON","SISTEMA","PROGRAMA")
 palabra=random.choice(palabras)
 
 # Desglosar la palabra
-deletreo=[]
+deletreo=[]  # lpaneque: Puedes convertir el string a list con list(palabra)
 for letra in palabra:
     deletreo.append(letra)
 
@@ -50,10 +50,10 @@ for letra in palabra:
 print(deletreo)
 
 # Presentación del juego
-print("-----:VAMOS A JUGAR AL AH_RCA_O:-----")
+print("-----:VAMOS A JUGAR AL AH_RCA_O:-----") # lpaneque: Mola
 
 # Pedir letra al jugador
-intento=input("Escribe una letra (en MAYUSCULAS): ")
+intento=input("Escribe una letra (en MAYUSCULAS): ") # lpaneque: la puedes convertir tú con upper(letra) y así te aseguras.
 
 # Compruebo que la letra(intento) elegida por el jugador está en la palabra
 if intento in deletreo:
@@ -68,3 +68,7 @@ if intento in deletreo:
 
 else:
     print("Mal")
+    
+# lpaneque: Te faltaría incluir esto en un bucle que se repita mientras no tengas 6 fallos, puedes guardar los fallos en una variable fallos.
+# lpaneque: Puedes utilizar esa variable fallos para imprimir el ahorcado correspondiente que está en una lista, ahorcado[0] sería la posición inicial,
+# lpaneque: ahorcado[1] sería la siguiente y así hasta llegar a la última que creo recordar que era ahorcado[6]
